38f056a4d3c22fa7fe6fe3d4573dd017f837d9e2
radomirkovacevic <73592072+radomirkovacevic@users.noreply.github.com>
2020-11-07 19:40:33 +0100

    Update cpanel.php

--- cpanel.php
+++ cpanel.php
@@ -1,62 +1,35 @@
 <?php
     session_start();
-    if(!isset($_SESSION['activated_user'])){
-?>
+    if(!isset($_SESSION['verified']) || $_SESSION['verified'] !== true){
+        header('Location: http://' . $_SERVER['HTTP_HOST']);
+        exit;
+    }
+    else{
+        $role;
+            foreach($_SESSION['activated_user'] as $aus){
+				$role= $aus[5];
+		    }
+		}
+?> 
 <!DOCTYPE html>
     <html lang="eng">
         <head>
             <meta charset="UTF-8">
             <title>Accounts</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <style>
-                    .register-page{
-                        width:100%;
-						margin:0px auto;
-						text-align:center;
-					}
-					.txt-registerYourself{}
-					.register-warning{
-						color:red;
-					}
-					.txt-loginYourself{}
-					input{
-						width:90%;
-					}
-				</style>
+                <style></style>
 		</head>
 		<body>
-		    <section>
-			    <div class="register-page">
-				    <h3 class="txt-registerYourself">Register yourself</h3>
-					<form class="form-register" action="/users/reg_user.php" method="post">
-						<label>What is your first name?</label><br>
-						<input type="text" name="firstnameSet"><br>
-						<label>What is your last name?</label><br>
-						<input type="text" name="lastnameSet"><br>
-						<label>Your mail is?</label><br>
-						<input type="text" name="mailSet"><br>
-						<label>Create password.</label><br>
-						<input type="password" name="passwordSet"><br>
-						<label class="register-warning">
-					   	 <?php if(isset($_SESSION['error'])){
-							       print $_SESSION['error'];
-								   session_destroy();
-								}
-							?>
-						</label><br>
-						<input class="submit" type="submit" name="insert"><br>
-					</form>
-					<h3 class="txt-loginYourself">Or</h3>
-					<div class="chose-login">
-						<a class="li-loginYourself" href="login.php">Login yourself</a>
-					</div>
-				</div>
-			</section>
-		</body>
-    </html>
-			<?php
-			    }else{
-					header('Location: http://' . $_SERVER['HTTP_HOST']);
-					exit;
-					}
-			?>
+      <section>
+        <div class="cpanel-area">
+        <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Welcome, <?php foreach($_SESSION['activated_user'] as $aus){print $aus[1] . ' ' . $aus[2];}  ?></h5>
+    <h6 class="card-subtitle mb-2 text-muted"><?php foreach($_SESSION['activated_user'] as $aus){print $aus[3];}  ?></h6>
+    <p class="card-text">Date of your registration: <br><?php foreach($_SESSION['activated_user'] as $aus){print $aus[4];}  ?></p>
+    <p class="card-text">Your role is: <br><?php foreach($_SESSION['activated_user'] as $aus){print $aus[5];}  ?></p>
+    <a href="index.php" class="card-link">Homepage</a>
+    <a href="logout.php" class="card-link">Logout</a>
+  </div>
+</div>
+      </section>
